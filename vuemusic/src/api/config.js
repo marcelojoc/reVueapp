c136e222e732a7d307fb0c582e58927d90c7a1d2
marceloNotebook <marcelojoc@gmail.com>
2019-01-28 16:13:25 -0300

    datos de api  lastFm  activos

---
+++ vuemusic/src/api/config.js
@@ -0,0 +1,19 @@
+
+export default{
+
+  appNmae: 'vuemusic',
+  apiKey: '37728cacbb1fe19e68c68e4a3e15efa6',
+  secret: '8ccb911a1534b1301c97eae4255845c8',
+  registred: 'marcelojoc',
+
+}
+
+
+
+
+// Here are the details of your new API account.
+
+// Application name	vuemusic
+// API key	37728cacbb1fe19e68c68e4a3e15efa6
+// Shared secret	8ccb911a1534b1301c97eae4255845c8
+// Registered to	marcelojoc
